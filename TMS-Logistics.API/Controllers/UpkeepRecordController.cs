ddbc7a7c2e91392a570ee84849a3ed50945f73f0
朱阳龙 <2239196410@qq.com>
2021-08-30 22:12:10 +0800

    8.30

--- TMS-Logistics.API/Controllers/UpkeepRecordController.cs
+++ TMS-Logistics.API/Controllers/UpkeepRecordController.cs
@@ -45,5 +45,104 @@ namespace TMS_Logistics.API.Controllers
                 throw;
             }
         }
+        /// <summary>
+        /// 保养记录添加
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult UpkeepRecordsAdd(UpkeepRecord obj)
+        {
+            try
+            {
+                int hang = Upkeep.UpkeepRecordsAdd(obj);
+                if (hang > 0)
+                {
+                    return Ok("添加成功");
+                }
+                else
+                {
+                    return Ok("添加失败");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "这里发生了错误");
+                throw;
+            }
+        }
+        /// <summary>
+        /// 保养记录删除
+        /// </summary>
+        /// <param name="UpkeepRecordID"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult UpkeepRecordsDel(string UpkeepRecordID)
+        {
+            try
+            {
+                int hang = Upkeep.UpkeepRecordsDel(UpkeepRecordID);
+                if (hang > 0)
+                {
+                    return Ok("删除成功");
+                }
+                else
+                {
+                    return Ok("删除失败");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "这里发生了错误");
+                throw;
+            }
+            
+        }
+        /// <summary>
+        /// 保养记录反填
+        /// </summary>
+        /// <param name="UpkeepRecordsDel"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult UpkeepRecordsDetails(int UpkeepRecordsDel)
+        {
+            try
+            {
+                return Ok(Upkeep.UpkeepRecordsDetails(UpkeepRecordsDel));
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "这里发生了错误");
+                throw;
+            }
+
+        }
+        /// <summary>
+        /// 保养记录修改
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult UpkeepRecordsUpd(UpkeepRecord obj)
+        {
+            try
+            {
+                int hang = Upkeep.UpkeepRecordsUpd(obj);
+                if (hang > 0)
+                {
+                    return Ok("修改成功");
+                }
+                else
+                {
+                    return Ok("修改失败");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "这里发生了错误");
+                throw;
+            }
+        }
+
     }
 }
